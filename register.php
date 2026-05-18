2519ef682b196afc0c8279bb64db51bdfc03b067
selvideaa <dearifaselvi@gmail.com>
2026-05-18 08:33:05 +0700

    Enhance registration with hashed password storage
    Updated password handling to use password_hash for security.

--- register.php
+++ register.php
@@ -1,27 +1,34 @@
 <?php
 include 'koneksi.php';
 
+
 if (isset($_POST['register'])) {
     $username = $_POST['username'];
-    $password = $_POST['password']; 
+   $password = password_hash($_POST['password'], PASSWORD_DEFAULT); 
     $nama     = $_POST['nama_lengkap'];
     $angkatan = $_POST['angkatan'];
     $jurusan  = $_POST['jurusan'];
 
-    $sql_users = "INSERT INTO users (username,password,role) VALUES ('$username','$password','user')";
-    if (mysqli_query($conn, $sql_users)) {
-        $user_id = mysqli_insert_id($conn);
+    $sql_users = "INSERT INTO users (username, password, role) 
+              VALUES ('$username', '$password', 'user')";
+   if (mysqli_query($conn, $sql_users)) {
+    // Ambil ID yang baru saja dibuat untuk tabel alumni
+    $user_id = mysqli_insert_id($conn);
 
-        $sql_alumni = "INSERT INTO alumni (nama_lengkap,angkatan,jurusan,user_id)
-                       VALUES ('$nama','$angkatan','$jurusan','$user_id')";
-        mysqli_query($conn, $sql_alumni);
+        // Query untuk memasukkan data ke tabel alumni
+    $sql_alumni = "INSERT INTO alumni (nama_lengkap, angkatan, jurusan, user_id) 
+                   VALUES ('$nama', '$angkatan', '$jurusan', '$user_id')";
+    
+    mysqli_query($conn, $sql_alumni);
+}
 
         header("Location: login.php");
         exit();
     } else {
-        echo "Error: " . mysqli_error($conn);
-    }
-}
+            $error = "gagal"; 
+        }
+    
+
 ?>
 
 <!DOCTYPE html>
@@ -53,17 +60,17 @@ if (isset($_POST['register'])) {
 
                 <div class="input-group">
                     <label>Nama Lengkap</label>
-                    <input type="text" name="Nama_Lengkap" placeholder="Tulis Nama Lengkap" required>
+                    <input type="text" name="nama_lengkap" placeholder="Tulis Nama Lengkap" required>
                 </div>
 
                 <div class="input-group">
                     <label>Angkatan</label>
-                    <input type="number" name="Angkatan" placeholder="Pilih Tahun" required>
+                    <input type="number" name="angkatan" placeholder="Pilih Tahun" required>
                 </div>
 
                 <div class="input-group">
                     <label>Jurusan</label>
-                    <select name="Jurusan">
+                    <select name="jurusan">
                         <option value="" disabled selected>Pilih Jurusan</option>
                         <option>Rekayasa Perangkat Lunak</option>
                         <option>Teknik Komputer dan Jaringan</option>
